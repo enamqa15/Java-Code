19643066765aaa7c8da4e24a29a8cf3815daaa62
sumaia <sumaia@sumaia-HP.gateway.pace.com>
2015-08-25 22:47:34 -0700

    I add super keyword program

---
+++ src/advancejava/SuperToyota.java
@@ -0,0 +1,21 @@
+package advancejava;
+
+
+	public class SuperToyota{
+		SuperToyota(){
+			System.out.println("My first car is a toyota corola");
+		}
+	}
+	class Corola extends SuperToyota{
+		Corola(){
+		super();
+			System.out.println("Corola car is ");
+		}
+	
+	
+	public static void main(String[] args) {
+		
+Corola x = new Corola();
+	}
+
+}
